8f594d7ad4dd6cfb4087b65fb16adb673cb7b1ed
blas <blasbenito@gmail.com>
2021-04-30 11:07:20 +0200

    a random commit

--- render.R
+++ render.R
@@ -1,3 +1,5 @@
 rmarkdown::render(input = "moiscrust.Rmd", output_format = "html_document", envir = new.env())
-rmarkdown::render(input = "moiscrust.Rmd", output_format = "pdf_document", envir = new.env()) 
+
+
+rmarkdown::render(input = "test.Rmd", output_format = "pdf_document", envir = new.env()) 
 
