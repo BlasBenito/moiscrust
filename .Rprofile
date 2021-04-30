0cde1929dad68f2a73cd84463dd413e49a45c35b
blas <blasbenito@gmail.com>
2021-04-30 13:43:03 +0200

    almost ready

--- .Rprofile
+++ .Rprofile
@@ -1,9 +1,1 @@
 source("renv/activate.R")
-# Add ./renv/bin to PATH, and override RStudio pandoc
-local({
-  bin <- normalizePath("renv/bin")
-  Sys.setenv(
-    PATH = paste(bin, Sys.getenv("PATH"), sep = ":"),
-    RSTUDIO_PANDOC = bin
-  )
-})
--- render.R
+++ render.R
@@ -1,5 +1,3 @@
 rmarkdown::render(input = "moiscrust.Rmd", output_format = "html_document", envir = new.env())
-
-
-rmarkdown::render(input = "test.Rmd", output_format = "pdf_document", envir = new.env()) 
+rmarkdown::render(input = "moiscrust.Rmd", output_format = "pdf_document", envir = new.env()) 
 
